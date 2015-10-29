dd5600c2cc5401f5a88007b249eccb22f95d4ab9
Bryan Gillespie <gillespb@gmail.com>
2015-10-29 22:07:30 +0000

    lesson files

---
+++ example4.16.pl
@@ -0,0 +1,7 @@
+@names=('John','Joe','Jake');
+print @names, "\n";         #print without separator
+print "Hi $names[0], $names[1], and $names[2]!\n";
+$number=@names;             #The scalar is assigned the umber of elements in the array
+
+print "There are $number elements in the \@names array.\n";
+print "The last element of the array is $names[$number -1].\n";
